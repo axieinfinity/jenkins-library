f725a4ec69c18f8f0abce23182bb8013d3d86e4c
Trung Nguyen <me@ngthanhtrung.com>
2019-10-12 05:02:48 +0700

    Support Discord notifications

---
+++ vars/discordNotify.groovy
@@ -0,0 +1,16 @@
+void call(Map parameters = [:]) {
+    withCredentials([usernamePassword(
+        credentialsId: parameters.credentialsId ?: 'discord_webhook',
+        usernameVariable: 'DISCORD_CHANNEL',
+        passwordVariable: 'DISCORD_WEBHOOK_TOKEN',
+    )]) {
+        discordSend(
+            webhookURL: "https://discordapp.com/api/webhooks/$DISCORD_CHANNEL/$DISCORD_WEBHOOK_TOKEN",
+            title: parameters.title,
+            link: parameters.link,
+            description: parameters.description,
+            footer: parameters.footer,
+            result: parameters.result,
+        )
+    }
+}
