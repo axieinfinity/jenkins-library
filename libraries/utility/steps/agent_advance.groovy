1295fe86f0d2339916e09414b180970db58684e7
namcaoxuan <caoxuannam.cxn@gmail.com>
2021-04-15 15:36:35 +0700

    New agent support yaml input

---
+++ libraries/utility/steps/agent_advance.groovy
@@ -0,0 +1,26 @@
+/* 
+  author: namcxn
+  date: Mar 02 00:00:00 +07 2021
+ */
+
+package libraries.utility
+
+import org.jenkinsci.plugins.workflow.steps.FlowInterruptedException
+import hudson.model.Result
+
+void call(Map parameters, Closure body) {
+  handleException {
+        githubAddTrigger()
+
+        podTemplate(parameters) {
+            githubSetStatus(status: 'PENDING', description: 'Waiting for Kubernetes build pod to be available')
+            node(POD_LABEL) {
+               body()
+            }
+        }
+    } { Exception exception ->
+        throw exception
+    } {
+        discord()
+    }
+}
