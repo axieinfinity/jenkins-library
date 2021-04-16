118ec38336c3c479080f907e5472c97bd76712f3
namcaoxuan <caoxuannam.cxn@gmail.com>
2021-04-16 18:24:24 +0700

    Update handle exception

--- libraries/utility/steps/agent_advance.groovy
+++ libraries/utility/steps/agent_advance.groovy
@@ -8,20 +8,31 @@ package libraries.utility
 import org.jenkinsci.plugins.workflow.steps.FlowInterruptedException
 import hudson.model.Result
 
-void call(Map parameters, Closure body) {
+void call(Map parameters, Closure body, Closure c4tch = null) {
   def label = "label-${UUID.randomUUID().toString()}"
-  handleException {
-        // githubAddTrigger()
 
+  try {
         podTemplate(label) {
             // githubSetStatus(status: 'PENDING', description: 'Waiting for Kubernetes build pod to be available')
             node(POD_LABEL) {
                body()
             }
         }
-    } { Exception exception ->
-        throw exception
-    } {
+    } catch (exception) {
+        if (exception instanceof FlowInterruptedException) {
+            currentBuild.result = ((FlowInterruptedException) exception).result.toString()
+        } else {
+            currentBuild.result = Result.FAILURE.toString()
+        }
+
+        println "Got exception: ${hudson.Functions.printThrowable(exception)}"
+
+        if (c4tch) {
+            c4tch(exception)
+        } else {
+            throw exception
+        }
+    } finally {
         discord()
     }
 }
