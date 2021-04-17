cf6c4293f3ff5414d6d20129125c61c411df0eb7
namcaoxuan <caoxuannam.cxn@gmail.com>
2021-04-17 13:22:00 +0700

    Format and lint

--- libraries/utility/steps/agent_advance.groovy
+++ libraries/utility/steps/agent_advance.groovy
@@ -9,54 +9,28 @@ import org.jenkinsci.plugins.workflow.steps.FlowInterruptedException
 import hudson.model.Result
 
 void call(Map parameters, Closure body, Closure c4tch = null) {
-  def label = "label-${UUID.randomUUID().toString()}"
+    def label = "label-${UUID.randomUUID().toString()}"
+    podTemplate(parameters) {
+        node(POD_LABEL) {
+            try {
+                body()
+            } catch (exception) {
+                if (exception instanceof FlowInterruptedException) {
+                    currentBuild.result = ((FlowInterruptedException) exception).result.toString()
+                } else {
+                    currentBuild.result = Result.FAILURE.toString()
+                }
 
-  podTemplate(parameters) {
-      node(POD_LABEL) {
-          try {
-              body()
-          } catch (exception) {
-            if (exception instanceof FlowInterruptedException) {
-            currentBuild.result = ((FlowInterruptedException) exception).result.toString()
-            } else {
-            currentBuild.result = Result.FAILURE.toString()
-            }
+                println "Got exception: ${hudson.Functions.printThrowable(exception)}"
 
-            println "Got exception: ${hudson.Functions.printThrowable(exception)}"
-
-            if (c4tch) {
-            c4tch(exception)
-            } else {
-            throw exception
-            } 
-          } finally {
-              discord()
-          }
-      }
-  }
-
-  try {
-        podTemplate(parameters) {
-            // githubSetStatus(status: 'PENDING', description: 'Waiting for Kubernetes build pod to be available')
-            node(label) {
-               body()
+                if (c4tch) {
+                    c4tch(exception)
+                } else {
+                    throw exception
+                } 
+            } finally {
+                discord()
             }
         }
-    } catch (exception) {
-        if (exception instanceof FlowInterruptedException) {
-            currentBuild.result = ((FlowInterruptedException) exception).result.toString()
-        } else {
-            currentBuild.result = Result.FAILURE.toString()
-        }
-
-        println "Got exception: ${hudson.Functions.printThrowable(exception)}"
-
-        if (c4tch) {
-            c4tch(exception)
-        } else {
-            throw exception
-        }
-    } finally {
-        discord()
     }
 }
