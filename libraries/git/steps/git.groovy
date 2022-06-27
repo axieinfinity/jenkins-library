c1f1bf566b8dec98e163e0954adf28aaca85e7a8
huy-axie <85155288+huy-axie@users.noreply.github.com>
2022-06-27 15:04:43 +0700

    Feat/support checkout tags (#25)
    * Support checkout tag.
    
    * Remove spaces.

--- libraries/git/steps/git.groovy
+++ libraries/git/steps/git.groovy
@@ -4,13 +4,13 @@ import org.codehaus.groovy.runtime.GStringImpl
 
 void call(Map args){
 
-  // supported git actions 
+  // supported git actions
   actions = [
     "add": { files ->
       if (files instanceof String) sh "git add ${files}"
       else if (files instanceof GStringImpl) sh "git add ${files}"
       else sh "git add ${files.join(" ")}"
-    }, 
+    },
     "commit": { message ->
       sh "git config user.email 'jenkins@axieinfinity.com' && git config user.name 'Jenkins'"
       sh script: "git commit -m \"${message}\"", returnStatus: true
@@ -26,16 +26,15 @@ void call(Map args){
   // validate that there are no invalid inputs
   invalid_args = args.findAll{ !(it.getKey() in actions.keySet()) }
   if (invalid_args) error "Unknown git actions: ${invalid_args.collect{ it.getKey() }.join(", ")}"
-  
+
   // validate the user is doing an action
   if (!(args.subMap(actions.keySet()))) error "git: You must use an action: ${actions.keySet().join(", ")}"
-  
-    
+
   // do the things.
   args.each{ action, value ->
     def c = actions.get(action)
     c.resolveStrategy = Closure.DELEGATE_FIRST
-    c.delegate = this        
+    c.delegate = this
     c.call(value)
   }
 }
--- libraries/git/steps/git_distributions.groovy
+++ libraries/git/steps/git_distributions.groovy
@@ -11,19 +11,19 @@ void call(){
     /*
       define a map of distributions and a closure for their own validations
     */
-    def options = [ 
-        "github": { c -> println "github config is ${c}"}, 
+    def options = [
+        "github": { c -> println "github config is ${c}"},
     ]
 
     def submap = config.subMap(options.keySet())
     if(submap.size() != 1){
-        error "you must configure one distribution option, currently: ${submap.keySet()}" 
+        error "you must configure one distribution option, currently: ${submap.keySet()}"
     }
-    
+
     // get the distribution
     String dist = submap.keySet().first()
-    // invoke the distribution closure 
-    options[dist](config[dist])    
+    // invoke the distribution closure
+    options[dist](config[dist])
 
     env.GIT_LIBRARY_DISTRUBITION = dist
     this.init_env()
@@ -31,23 +31,15 @@ void call(){
 
 // Initialize Git configuration of env vars
 void init_env(){
-    //node{
-        // try { unstash "workspace" }
-        // catch(ignored) { 
-        //   println "'workspace' stash not present. Skipping git library environment variable initialization. To change this behavior, ensure the 'sdp' library is loaded"
-        //   return
-        // }
     stage "Checkout source code", {
         cleanWs()
         try{
             checkout scm
         }catch(AbortException ex) {
-            println "scm var not present, skipping source code checkout" 
+            println "scm var not present, skipping source code checkout"
         }catch(err){
-          println "exception ${err}" 
+          println "exception ${err}"
         }
-        
-        // stash name: 'workspace', allowEmpty: true, useDefaultExcludes: false
 
         env.GIT_URL = scm.getUserRemoteConfigs()[0].getUrl()
         env.GIT_CREDENTIAL_ID = scm.getUserRemoteConfigs()[0].credentialsId.toString()
@@ -56,11 +48,18 @@ void init_env(){
             parts = parts.drop(1)
             if (part.contains(".")) break
         }
+
         env.ORG_NAME = parts.getAt(0)
         env.REPO_NAME = parts[1..-1].join("/") - ".git"
 
         def target_branch_name = env.BRANCH_NAME
-        env.GIT_SHA = sh(script: "git rev-parse --short remotes/origin/${target_branch_name}", returnStdout: true).trim()
+
+       // Check if checking out a tags or a branch
+        if (isTags()){
+            env.GIT_SHA = sh(script: "git rev-parse --short refs/tags/${target_branch_name}", returnStdout: true).trim()
+        }else{
+            env.GIT_SHA = sh(script: "git rev-parse --short remotes/origin/${target_branch_name}", returnStdout: true).trim()
+        }
 
         if (env.CHANGE_TARGET){
             env.GIT_BUILD_CAUSE = "pr"
@@ -95,6 +94,19 @@ boolean isUpdatedWithTarget() {
     String headOrigin = sh(script: "git rev-parse remotes/origin/${env.CHANGE_TARGET}", returnStdout: true).trim()
 
     if (headOrigin.equals(gitMergeBaseCommit)){
+        return true
+    }
+    return false
+}
+
+// Check if this is a tag
+boolean isTags() {
+    // Always return true
+    String currentTag = sh(script:"git describe --tags || true", returnStdout: true).trim()
+
+    String currtentBranch = env.BRANCH_NAME
+
+    if (currentTag.equals(currtentBranch)){
         return true
     }
     return false
